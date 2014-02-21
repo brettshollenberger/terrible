8b005a5249b222d37f9bc1ca442a2d19c22a3318
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-02-21 00:11:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TerribleTracker::Application.config.secret_key_base = '57180d88f37182bf2e0fbb64b5791eda523385461cd393dfccdfd321796a4297ae2ebed3b610183b28d01c171fa718f462e61b3c51f52b0473538b70ea5a5b0b'
