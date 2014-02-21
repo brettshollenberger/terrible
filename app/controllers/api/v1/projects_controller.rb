18259705e8fa6beeee83b201c2a34842ac4eadc2
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-02-21 02:02:35 -0500

    Add Show action to Project API

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -7,6 +7,17 @@ module Api
       def index
         respond_with(current_user.projects)
       end
+
+      def show
+        @project = current_user.projects.where(id: params[:id]).first
+        if @project
+          respond_with(@project)
+        else
+          render :json => {success: false, 
+                           message: "You don't have permission to view that resource"},
+                           :status => "401"
+        end
+      end
     end
   end
 end
--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -2,35 +2,73 @@ require 'spec_helper'
 
 describe "Projects API" do
 
-  describe "when not logged in" do
-    before(:each) do
-      FactoryGirl.create_list(:project, 10)
-      get "/api/v1/projects.json"
+  describe "index" do
+    describe "when not logged in" do
+      before(:each) do
+        FactoryGirl.create_list(:project, 10)
+        get "/api/v1/projects.json"
+      end
+
+      it "is not a successful request" do
+        expect(response).to_not be_success
+      end
+
+      it "responds with an error message" do
+        expect(json["error"]).to eq("You need to sign in or sign up before continuing.")
+      end
     end
 
-    it "is not a successful request" do
-      expect(response).to_not be_success
-    end
+    describe "when logged in" do
+      before(:each) do
+        FactoryGirl.create_list(:project, 10)
+        FactoryGirl.create(:user_project_collaboration, collaborator: user)
+        login(user)
+        get "/api/v1/projects.json"
+      end
 
-    it "responds with an error message" do
-      expect(json["error"]).to eq("You need to sign in or sign up before continuing.")
+      it "is a successful request" do
+        expect(response).to be_success
+      end
+
+      it "responds with a list of projects the user collaborates on" do
+        expect(json.length).to eq(1)
+      end
     end
   end
 
-  describe "when logged in" do
-    before(:each) do
-      FactoryGirl.create_list(:project, 10)
-      FactoryGirl.create(:user_project_collaboration, collaborator: user)
-      login(user)
-      get "/api/v1/projects.json"
+  describe "show" do
+    describe "when logged in and able to view the project" do
+      before(:each) do
+        @upc = FactoryGirl.create(:user_project_collaboration, collaborator: user)
+        @project = @upc.collaboratable
+        login(user)
+        get "/api/v1/projects/#{@project.id}.json"
+      end
+
+      it "is a successful request" do
+        expect(response).to be_success
+      end
+
+      it "responds with the user's project, provided they are a collaborator" do
+        expect(json["title"]).to eql(@project.title)
+      end
     end
 
-    it "is a successful request" do
-      expect(response).to be_success
-    end
+    describe "when logged in and not able to view the project" do
+      before(:each) do
+        @upc = FactoryGirl.create(:user_project_collaboration)
+        @project = @upc.collaboratable
+        login(user)
+        get "/api/v1/projects/#{@project.id}.json"
+      end
 
-    it "responds with a list of projects" do
-      expect(json.length).to eq(1)
+      it "is a successful request" do
+        expect(response).to_not be_success
+      end
+
+      it "responds with the user's project, provided they are a collaborator" do
+        expect(json["message"]).to eql("You don't have permission to view that resource")
+      end
     end
   end
 end
