d8d73b8472e233a7b872476f5b22fa6616f5441e
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-03-19 23:15:11 -0400

    Remove old Projects Controller methods that are replaced by the ApiController's metaprogramming.

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,56 +1,6 @@
 module Api
   module V1
     class ProjectsController < ApiController
-      def show
-        @project = current_user.projects.where(id: params[:id]).first
-        if @project
-          respond_with(@project)
-        else
-          render :json => {success: false, 
-                           message: "You don't have permission to view that resource"},
-                           :status => "401"
-        end
-      end
-
-      def create
-        begin
-          @project = current_user.projects.new(project_params)
-        rescue ActionController::ParameterMissing
-          @project = current_user.projects.new
-        end
-
-        @collaboratorship = Collaboratorship.new(collaborator: current_user,
-                                                 collaboratable: @project,
-                                                 role: "owner",
-                                                 state: "active")
-
-        if @project.save && @collaboratorship.save
-          render :json => @project, status: :created, location: api_v1_project_url(@project)
-        else
-          render :json => @project.errors, status: :unprocessable_entity
-        end
-      end
-
-      def update
-        begin
-          @project = current_user.projects.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-          begin
-            @project = Project.find(params[:id])
-          rescue ActiveRecord::RecordNotFound
-            render :json => {success: false,
-                             message: "Resource not found",
-                             status: "404"}, status: :not_found and return
-          end
-          render :json => {success: false,
-                           message: "You don't have permission to update that resource",
-                           status: "401"}, status: :unauthorized and return
-        end
-
-        if @project.update(project_params)
-          render :json => @project, status: :accepted, location: api_v1_project_url(@project)
-        end
-      end
 
     private
       def project_params
